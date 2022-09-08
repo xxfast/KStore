e06e985ab0813d0f92b44e99d96258215f6270bf
Isuru Rajapakse <isuru@xfastgames.com>
2022-09-09 06:59:26 +1000

    Add android target

---
+++ KStore/build.gradle.kts
@@ -0,0 +1,118 @@
+plugins {
+  kotlin("multiplatform")
+  kotlin("plugin.serialization")
+  id("com.android.library")
+}
+
+group = "io.github.xxfast"
+version = "0.1-SNAPSHOT"
+
+repositories {
+  mavenCentral()
+}
+
+android {
+  compileSdk = 31
+  defaultConfig {
+    minSdk = 21
+    targetSdk = 31
+  }
+
+  sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
+
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+  }
+}
+
+kotlin {
+  android {
+    compilations.all {
+      kotlinOptions {
+        jvmTarget = "1.8"
+      }
+    }
+  }
+
+  jvm("desktop") {
+    compilations.all {
+      kotlinOptions.jvmTarget = "1.8"
+    }
+    testRuns["test"].executionTask.configure {
+      useJUnitPlatform()
+    }
+  }
+
+  js(IR) {
+    browser {
+      commonWebpackConfig {
+        cssSupport.enabled = true
+      }
+    }
+  }
+
+  val hostOs = System.getProperty("os.name")
+  val isMingwX64 = hostOs.startsWith("Windows")
+  val nativeTarget = when {
+    hostOs == "Mac OS X" -> macosX64("native")
+    hostOs == "Linux" -> linuxX64("native")
+    isMingwX64 -> mingwX64("native")
+    else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
+  }
+
+
+  sourceSets {
+    val commonMain by getting {
+      dependencies {
+        implementation("com.squareup.okio:okio:3.2.0")
+        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+        implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.0")
+        implementation("org.jetbrains.kotlinx:kotlinx-serialization-json-okio:1.4.0")
+
+      }
+    }
+
+    val commonTest by getting {
+      dependsOn(commonMain)
+      dependencies {
+        implementation(kotlin("test"))
+        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.4")
+        implementation("app.cash.turbine:turbine:0.9.0")
+      }
+    }
+
+    val androidMain by getting
+    val androidTest by getting {
+      dependsOn(androidMain)
+    }
+
+    val desktopMain by getting {
+      dependencies {
+        implementation("com.squareup.okio:okio:3.2.0")
+      }
+    }
+    val desktopTest by getting {
+      dependsOn(desktopMain)
+    }
+
+    val jsMain by getting {
+      dependencies {
+        implementation("com.squareup.okio:okio:3.2.0")
+      }
+    }
+    val jsTest by getting {
+      dependsOn(jsMain)
+    }
+
+    val nativeMain by getting {
+      dependencies {
+        implementation("com.squareup.okio:okio:3.2.0")
+      }
+    }
+
+    val nativeTest by getting {
+      dependsOn(nativeMain)
+    }
+  }
+}
