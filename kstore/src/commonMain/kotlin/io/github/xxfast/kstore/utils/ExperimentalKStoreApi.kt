c4cc72c83a9dc8afde63d388666a2646e423448c
xxfast <isurukusumal36@gmail.com>
2023-02-13 09:56:36 +1100

    Add experimental store api annotations

---
+++ kstore/src/commonMain/kotlin/io/github/xxfast/kstore/utils/ExperimentalKStoreApi.kt
@@ -0,0 +1,6 @@
+package io.github.xxfast.kstore.utils
+
+@RequiresOptIn(message = "This API is experimental. It may be changed in the future without notice.")
+@Retention(AnnotationRetention.BINARY)
+@Target(AnnotationTarget.CLASS, AnnotationTarget.FUNCTION)
+annotation class ExperimentalKStoreApi
