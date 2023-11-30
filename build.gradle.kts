a50f4760c50edccb9be2497e9d5fcaa266491756
xxfast <isurukusumal36@gmail.com>
2023-11-30 14:00:04 +1100

    Bump dokka

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
 
 plugins {
   id("org.jetbrains.kotlinx.kover") version "0.6.1"
-  id("org.jetbrains.dokka") version "1.8.10"
+  id("org.jetbrains.dokka") version "1.9.10"
 }
 
 buildscript {
