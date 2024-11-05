1fc00055f9cc6e538073222a7e2c6675ae50d9a4
xxfast <isurukusumal36@gmail.com>
2024-11-06 10:23:32 +1100

    Prepare for snapshot 0.9.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,7 @@ allprojects {
   }
 
   group = "io.github.xxfast"
-  version = "1.0.0-SNAPSHOT"
+  version = "0.9.1-SNAPSHOT"
 
   apply(plugin = "org.jetbrains.kotlin.plugin.serialization")
   apply(plugin = "org.jetbrains.kotlinx.kover")
