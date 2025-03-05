589b7af498ea7e92ffbe1ffe249b2cae06fd3bb7
Shreyash Kore <kore.shreyash@gmail.com>
2025-03-05 22:57:57 +0530

    implement InMemoryCodec

---
+++ kstore-testing/src/commonMain/kotlin/io/github/xxfast/kstore/testing/InMemoryCodec.kt
@@ -0,0 +1,36 @@
+package io.github.xxfast.kstore.testing
+
+import io.github.xxfast.kstore.Codec
+import io.github.xxfast.kstore.DefaultJson
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.StringFormat
+import kotlinx.serialization.serializer
+
+
+/**
+ * A codec that stores data in memory.
+ */
+public inline fun <reified T : @Serializable Any> InMemoryCodec(
+  format: StringFormat = DefaultJson,
+): InMemoryCodec<T> = InMemoryCodec(format, format.serializersModule.serializer())
+
+/**
+ * A codec that stores data in memory.
+ */
+public class InMemoryCodec<T : @Serializable Any>(
+  private val format: StringFormat,
+  private val serializer: KSerializer<T>
+) : Codec<T> {
+
+  private var storedData: String? = null
+
+  override suspend fun encode(value: T?) {
+    storedData = value?.let { format.encodeToString(serializer, it) }
+  }
+
+  override suspend fun decode(): T? = storedData?.let {
+    format.decodeFromString(serializer, it)
+  }
+
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,3 +3,4 @@ rootProject.name = "KStore"
 include(":kstore")
 include(":kstore-file")
 include(":kstore-storage")
+include(":kstore-testing")
